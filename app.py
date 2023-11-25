a46eb1e3872abe917f4c37f8ef73684a648c21b5
minich-code <minichworks@gmail.com>
2023-11-25 04:33:50 +0300

    the next commit

--- app.py
+++ app.py
@@ -1,18 +1,44 @@
 import pickle
-from flask import Flask, request, app, jasonify, url_for, render_template
+from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np 
 import pandas as pd 
+import joblib
 
-
+# Create app with Flask 
 app = Flask(__name__)
-# Load the model 
-linear_regression_model = pickle.load('linear_regression_model.pkl', 'rb')
 
+# Load the pickle file 
+linear_regression_model = joblib.load('linear_regression_model.pkl')
+scalar = joblib.load('scalar.pkl')
+
+
+# app.route 
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data = request.json['data']
-    print(data)
+    try:
+        # Get JSON data from the request
+        data = request.json['data']
+        
+        # Print and preprocess the input data
+        print("Received Data:", data)
+        new_data = scalar.transform(np.array(list(data.values())).reshape(1, -1))
+
+        # Make predictions using the loaded model
+        predictions = linear_regression_model.predict(new_data)
+        print("Predictions:", predictions)
+
+        # Convert predictions to a list (assuming 'predictions' is a NumPy array)
+        output = predictions.tolist()
+
+        return jsonify({'predictions': output})
+    
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
