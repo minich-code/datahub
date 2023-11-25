ff07a52bcc15467ede0c1a2c944240a69b34cb47
minich-code <minichworks@gmail.com>
2023-11-25 05:25:13 +0300

    The web app is ready

--- app.py
+++ app.py
@@ -38,6 +38,24 @@ def predict_api():
     
     except Exception as e:
         return jsonify({'error': str(e)})
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Get form data from the request and convert it to a list of floats
+    data = [float(x) for x in request.form.values()]
+
+    # Transform the input data using the loaded scalar
+    final_input = scalar.transform(np.array(data).reshape(1, -1))
+
+    # Print the transformed input data (optional, for debugging)
+    print(final_input)
+
+    # Make a prediction using the loaded linear regression model
+    output = linear_regression_model.predict(final_input)[0]
+
+    # Render the prediction on the 'home.html' template
+    return render_template("home.html", prediction_text="The house price prediction is {}".format(output))        
     
 
 if __name__ == '__main__':
